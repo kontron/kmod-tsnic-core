13c57edccc6c65e957466d51be880e375ea2c560
Michael Walle <michael.walle@kontron.com>
2018-05-28 15:16:46 +0200

    major redesign
    Register individual devices for the DEIP part. Use properties for the
    mac address passing. Use the new DEIP 1.3.0 address layout.
    
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- tsnic-core-pci.c
+++ tsnic-core-pci.c
@@ -27,6 +27,7 @@
 #include <linux/module.h>
 #include <linux/mfd/core.h>
 #include <linux/if_ether.h>
+#include <linux/property.h>
 
 #include "tsnic-core.h"
 
@@ -49,20 +50,162 @@ enum tsnic_mfd_irqs {
 static void __iomem *bar5_virt = NULL;
 static u8 eth_addr[ETH_ALEN];
 
-static struct resource tsnic_mfd_resources[NR_BARS][MAX_RES];
-
-static struct mfd_cell tsnic_mfd_cells[] = {
+static struct resource tsnic_tse_resources[] = {
 	{
-		.id = TSE_BAR,
-		.name = "tsnic-tse",
-		.num_resources = 4,
-		.resources = &tsnic_mfd_resources[TSE_BAR][0],
+		.start = 0,
+		.end = 0xfff,
+		.flags = IORESOURCE_MEM,
 	},
 	{
-		.id = SWITCH_BAR,
-		.name = "tsnic-deip",
-		.num_resources = 4,
-		.resources = &tsnic_mfd_resources[SWITCH_BAR][0],
+		.start = TSE_IRQ0,
+		.end = TSE_IRQ0,
+		.flags = IORESOURCE_IRQ,
+	},
+	{
+		.start = TSE_IRQ1,
+		.end = TSE_IRQ1,
+		.flags = IORESOURCE_IRQ,
+	},
+};
+
+/* deipce */
+static struct resource deipce_resources[] = {
+	{
+		.start = 0x3000000,
+		.end =   0x30fffff,
+		.flags = IORESOURCE_MEM,
+	},
+	{
+		.start = DEIP_IRQ0,
+		.end = DEIP_IRQ0,
+		.flags = IORESOURCE_IRQ,
+	},
+};
+
+/* deipce-mdio */
+static struct resource deipce_mdio_resources[] = {
+	{
+		.start = 0x4000000,
+		.end = 0x40000ff,
+		.flags = IORESOURCE_MEM,
+	},
+};
+
+/* flx_ac */
+static struct resource flx_ac_resources[] = {
+	{
+		.start = 0x2000000,
+		.end = 0x200ffff,
+		.flags = IORESOURCE_MEM,
+	},
+};
+
+/* flx_ibc */
+static struct resource flx_ibc_resources[] = {
+	{
+		.start = 0x2100000,
+		.end = 0x210ffff,
+		.flags = IORESOURCE_MEM,
+	},
+};
+
+/* flx_fpts */
+static struct resource flx_fpts0_resources[] = {
+	{
+		.start = 0x2120000,
+		.end = 0x212ffff,
+		.flags = IORESOURCE_MEM,
+	},
+};
+
+/* flx_frtc */
+static struct resource flx_frtc0_resources[] = {
+	{
+		.start = 0x2180000,
+		.end = 0x218ffff,
+		.flags = IORESOURCE_MEM,
+	},
+};
+static struct resource flx_frtc1_resources[] = {
+	{
+		.start = 0x2190000,
+		.end = 0x219ffff,
+		.flags = IORESOURCE_MEM,
+	},
+};
+
+/* flx_fsc */
+static struct resource flx_fsc_resources[] = {
+	{
+		.start = 0x21c0000,
+		.end = 0x21cffff,
+		.flags = IORESOURCE_MEM,
+	},
+};
+
+static struct property_entry generic_properties[] = {
+		PROPERTY_ENTRY_U8_ARRAY("mac-address", eth_addr),
+		{}
+};
+
+static struct mfd_cell tsnic_bar0[] = {
+	{
+		.id = 1,
+		.name = "deipce",
+		.num_resources = ARRAY_SIZE(deipce_resources),
+		.resources = deipce_resources,
+		.properties = generic_properties,
+	},
+	{
+		.name = "flx_ac",
+		.id = 1,
+		.num_resources = ARRAY_SIZE(flx_ac_resources),
+		.resources = flx_ac_resources,
+	},
+	{
+		.name = "flx_ibc",
+		.id = 1,
+		.num_resources = ARRAY_SIZE(flx_ibc_resources),
+		.resources = flx_ibc_resources,
+	},
+	{
+		.name = "flx_fpts",
+		.id = 1,
+		.num_resources = ARRAY_SIZE(flx_fpts0_resources),
+		.resources = flx_fpts0_resources,
+	},
+	{
+		.name = "flx_frtc",
+		.id = 1,
+		.num_resources = ARRAY_SIZE(flx_frtc0_resources),
+		.resources = flx_frtc0_resources,
+	},
+	{
+		.name = "flx_frtc",
+		.id = 2,
+		.num_resources = ARRAY_SIZE(flx_frtc1_resources),
+		.resources = flx_frtc1_resources,
+	},
+	{
+		.id = 1,
+		.name = "flx_fsc",
+		.num_resources = ARRAY_SIZE(flx_fsc_resources),
+		.resources = flx_fsc_resources,
+	},
+	{
+		.id = 1,
+		.name = "deipce-mdio",
+		.num_resources = ARRAY_SIZE(deipce_mdio_resources),
+		.resources = deipce_mdio_resources,
+	},
+};
+
+static struct mfd_cell tsnic_bar1[] = {
+	{
+		.name = "tsnic-tse",
+		.num_resources = ARRAY_SIZE(tsnic_tse_resources),
+		.resources = tsnic_tse_resources,
+		.properties = generic_properties,
 	},
 };
 
@@ -77,8 +220,8 @@ static void tsnic_setup_vpd(struct pci_dev *pdev)
 
 static int tsnic_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
-	int err, i;
-	struct resource *r;
+	int err;
+	int irqbase;
 
 	err = pci_enable_device(pdev);
 	if (err)
@@ -104,57 +247,36 @@ static int tsnic_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent
 			eth_addr[0], eth_addr[1], eth_addr[2],
 			eth_addr[3], eth_addr[4], eth_addr[5]);
 
-	/* fill in IO range for each cell; subdrivers handle the region */
-	for (i = 0; i < ARRAY_SIZE(tsnic_mfd_cells); i++) {
-		int bar = tsnic_mfd_cells[i].id;
-		r = &tsnic_mfd_resources[bar][0];
-
-		r->flags = IORESOURCE_MEM;
-		r->start = pci_resource_start(pdev, bar);
-		r->end = pci_resource_end(pdev, bar);
-
-		/* eth addr */
-		r = &tsnic_mfd_resources[bar][1];
-		r->flags = IORESOURCE_REG;
-		r->start = (unsigned long)&eth_addr[0];
-		r->end = (unsigned long)&eth_addr[6];
-
-		/* id is used for temporarily storing BAR; unset it now */
-		tsnic_mfd_cells[i].id = 0;
-	}
-
 	if (pci_alloc_irq_vectors(pdev, 1, 32, PCI_IRQ_MSIX) != 4) {
 		// TODO: fall back to MSI or legacy interrupts
 		goto err_disable;
 	}
+	irqbase = pci_irq_vector(pdev, 0);
 
-	r = &tsnic_mfd_resources[0][2];
-	r->flags = IORESOURCE_IRQ;
-	r->start = r->end = pci_irq_vector(pdev, DEIP_IRQ0);
-
-	r = &tsnic_mfd_resources[0][3];
-	r->flags = IORESOURCE_IRQ;
-	r->start = r->end = pci_irq_vector(pdev, DEIP_IRQ1);
-
-	r = &tsnic_mfd_resources[1][2];
-	r->flags = IORESOURCE_IRQ;
-	r->start = r->end = pci_irq_vector(pdev, TSE_IRQ1);
-
-	r = &tsnic_mfd_resources[1][3];
-	r->flags = IORESOURCE_IRQ;
-	r->start = r->end = pci_irq_vector(pdev, TSE_IRQ0);
-
-	err = mfd_add_devices(&pdev->dev, -1, tsnic_mfd_cells,
-			      ARRAY_SIZE(tsnic_mfd_cells), NULL, 0, NULL);
+	err = mfd_add_devices(&pdev->dev, -1, tsnic_bar0,
+			      ARRAY_SIZE(tsnic_bar0), &pdev->resource[0], irqbase, NULL);
 	if (err) {
 		dev_err(&pdev->dev, "MFD add devices failed: %d\n", err);
-		goto err_disable;
+		goto err_free;
+	}
+
+	err = mfd_add_devices(&pdev->dev, -1, tsnic_bar1,
+			      ARRAY_SIZE(tsnic_bar1), &pdev->resource[1], irqbase, NULL);
+	if (err) {
+		dev_err(&pdev->dev, "MFD add devices failed: %d\n", err);
+		goto err_remove_devices;
 	}
 
 	dev_info(&pdev->dev, "%zu devices registered.\n",
-			ARRAY_SIZE(tsnic_mfd_cells));
+			ARRAY_SIZE(tsnic_bar0) + ARRAY_SIZE(tsnic_bar1));
 
 	return 0;
+
+err_remove_devices:
+	mfd_remove_devices(&pdev->dev);
+
+err_free:
+	pci_free_irq_vectors(pdev);
 
 err_disable:
 	pci_disable_device(pdev);
